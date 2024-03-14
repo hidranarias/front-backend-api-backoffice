c8f1f56ad9fdbcb14590ff6f3e34ab727e5e69be
Hidran Arias <hidran.arias@spryker.com>
2024-03-14 11:34:07 +0100

    Refactors Toolbox command script

--- src/Pyz/Zed/Toolbox/Communication/Console/GenerateBackendApiModuleConsole.php
+++ src/Pyz/Zed/Toolbox/Communication/Console/GenerateBackendApiModuleConsole.php
@@ -2,7 +2,6 @@
 
 declare(strict_types=1);
 
-
 namespace Pyz\Zed\Toolbox\Communication\Console;
 
 use Spryker\Zed\Kernel\Communication\Console\Console;
@@ -15,110 +14,79 @@ use Symfony\Component\Console\Output\OutputInterface;
  */
 class GenerateBackendApiModuleConsole extends Console
 {
-    public const COMMAND_NAME = 'toolbox:generate-module';
-    public const DESCRIPTION = "Generates a  module from a YML configuration or name.
+    protected const COMMAND_NAME = 'toolbox:generate-module';
+    protected const DESCRIPTION = "Generates a module from a YML configuration or name.
     \n Specify a type and a name or config file";
-    public const OPTION_CONFIG = 'config';
-    public const OPTION_CONFIG_SHORTCUT = 'c';
-    public const OPTION_NAME = 'name';
-    public const OPTION_NAME_SHORTCUT = 'm';
-    public const OPTION_TYPE = 'type';
-    public const OPTION_TYPE_SHORTCUT = 't';
+    protected const OPTION_CONFIG = 'config';
+    protected const OPTION_NAME = 'name';
+    protected const OPTION_TYPE = 'type';
 
-    /**
-     * This method configures the console command. It sets the name, description, and options.
-     */
     protected function configure(): void
     {
         $this
             ->setName(static::COMMAND_NAME)
             ->setAliases(['tgm', 't:g:m', 'tbgm'])
             ->setDescription(static::DESCRIPTION)
-            ->addOption(
-                static::OPTION_CONFIG,
-                static::OPTION_CONFIG_SHORTCUT,
-                InputOption::VALUE_OPTIONAL,
-                'The path to the YAML configuration file.'
-            )->addOption(
-                static::OPTION_NAME,
-                static::OPTION_NAME_SHORTCUT,
-                InputOption::VALUE_OPTIONAL,
-                'The module\'s name'
-            )
-            ->addOption(
-                static::OPTION_TYPE,
-                static::OPTION_TYPE_SHORTCUT,
-                InputOption::VALUE_REQUIRED,
-                'The type of the module (Zed, Yves, Client, FrontendApi, BackendApi).'
-            );
+            ->addOption(static::OPTION_CONFIG, 'c', InputOption::VALUE_OPTIONAL,
+                'The path to the YAML configuration file.')
+            ->addOption(static::OPTION_NAME, 'm', InputOption::VALUE_OPTIONAL, 'The module\'s name')
+            ->addOption(static::OPTION_TYPE, 't', InputOption::VALUE_REQUIRED,
+                'The type of the module (Zed, Yves, Client, FrontendApi, BackendApi).');
+
         parent::configure();
     }
 
-    /**
-     * This method is executed when the console command is called. It handles the logic of reading the YAML configuration,
-     * generating the module, and outputting messages to the console.
-     *
-     * @param \Symfony\Component\Console\Input\InputInterface $input An InputInterface instance
-     * @param \Symfony\Component\Console\Output\OutputInterface $output An OutputInterface instance
-     *
-     * @return int Return code of the command execution
-     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        $type = $input->getOption(static::OPTION_TYPE);
+        if (!$type) {
+            return $this->outputError($output, 'Please, specify a type: (Zed, Yves, Client, FrontendApi, BackendApi)');
+        }
+
         $configFilePath = $input->getOption(static::OPTION_CONFIG);
         $name = $input->getOption(static::OPTION_NAME);
-        $type = $input->getOption(static::OPTION_TYPE);
 
-        if (!$type) {
-            $output->writeln('<error>Please, specify a type: (Zed, Yves, Client, FrontendApi, BackendApi)</error>');
-            return static::FAILURE;
-        }
         if ($configFilePath && !file_exists($configFilePath)) {
-            $output->writeln(sprintf('<error>The configuration file "%s" does not exist.</error>', $configFilePath));
-            return static::FAILURE;
+            return $this->outputError($output, sprintf('The configuration file "%s" does not exist.', $configFilePath));
         }
+
         if (!$configFilePath && !$name) {
-            $output->writeln('<error>Either config file or module name has to be provided</error>');
-            return static::FAILURE;
+            return $this->outputError($output, 'Either config file or module name has to be provided');
         }
+
+        return $this->generateModule($type, $configFilePath, $name, $output);
+    }
+
+    protected function outputError(OutputInterface $output, string $message): int
+    {
+        $output->writeln(sprintf('<error>%s</error>', $message));
+        return static::FAILURE;
+    }
+
+    protected function generateModule(
+        string $type,
+        ?string $configFilePath,
+        ?string $name,
+        OutputInterface $output
+    ): int {
         try {
-            $moduleType = strtolower($type);
-            switch (strtolower($moduleType)) {
+            switch (strtolower($type)) {
                 case 'zed':
-                    if ($configFilePath) {
-                        $this->getFacade()->generateZedModuleFromConfig($configFilePath);
-                    } else {
-                        $this->getFacade()->generateZedModuleFromName($name);
-                    }
-                    break;
                 case 'yves':
-                    $this->getFacade()->generateYvesModuleFromConfig($configFilePath);
-                    break;
                 case 'client':
-                    $this->getFacade()->generateClientModuleFromConfig($configFilePath);
-                    break;
                 case 'frontendapi':
-                case 'api':
-                    $this->getFacade()->generateFrontendApiModuleFromConfig($configFilePath);
-                    break;
                 case 'backendapi':
-                case 'backend':
-                default:
-                    if ($configFilePath) {
-                        $this->getFacade()->generateBackendApiModuleFromConfig($configFilePath);
-                    } else {
-                        $this->getFacade()->generateBackendApiModuleFromName($name);
-                    }
+                    $methodName = 'generate' . ucfirst($type) . 'ModuleFrom' . ($configFilePath ? 'Config' : 'Name');
+                    $this->getFacade()->$methodName($configFilePath ?? $name);
                     break;
+                default:
+                    return $this->outputError($output, 'Invalid module type specified.');
             }
         } catch (\Exception $e) {
-            $output->writeln(sprintf('<error>%s</error>', $e->getMessage()));
-            return static::FAILURE;
+            return $this->outputError($output, $e->getMessage());
         }
 
-
         $output->writeln('<info>Module generated successfully.</info>');
-
         return static::SUCCESS;
     }
 }
